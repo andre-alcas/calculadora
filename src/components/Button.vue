ed2f82fa35af4e1cfa8c8068483a0ce0f1760543
andre-alcas <alcas.andre@gmail.com>
2022-06-01 11:49:48 -0300

    calculadora1

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <Button class="button" :class="{ double, triple, operation }">
+    {{ label }}
+  </Button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {},
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
+  },
+};
+</script>
+
+<style>
+:root{
+    --bg-button: #f0f0f0;
+}
+
+.button.double{
+    grid-column: span 2;
+}
+.button.triple{
+    grid-column: span 3;
+}
+.button.operation{
+    background-color: #f0f0f0;
+}
+
+</style>
---
+++ src/components/Display.vue
@@ -0,0 +1,17 @@
+<template>
+  <display class="display">
+    {{ value }}
+  </display>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+};
+</script>
+
+<style>
+.display {
+  grid-column: span 4;
+}
+</style>
